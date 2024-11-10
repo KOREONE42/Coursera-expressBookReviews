4bc7801683a2d0edac012227d3e317408685035f
KOREONE42 <kore@kore.one>
2024-11-10 18:02:41 +0100

    Initial Push

--- final_project/index.js
+++ final_project/index.js
@@ -3,32 +3,32 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-const jwtSecret = '244d0b97c61cb978567e348a15fc8cd5c3c5791af982ccae88db48383bc3c273';
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",session({secret:"secretKey",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).json({ message: "No token provided" });
-    }
+  // Check if the user has an access token in the session
+  if (!req.session.accessToken) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-    const token = authHeader.split(' ')[1]; // Bearer <token>
+  // Verify the access token and extract the user password from it
+  const accessToken = req.session.accessToken;
+  try {
+    const decodedToken = jwt.verify(accessToken, "secretKey");
+    const userPassword = decodedToken.userPassword;
+    req.userPassword = userPassword;
+    next();
+  } catch (err) {
+    return res.status(401).json(err);
 
-    // Verify the token
-    jwt.verify(token, jwtSecret, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: "Invalid token" });
-        }
+    //return res.status(401).json({accessToken:req.session.accessToken, message: "Invalid access token" });
+  }
 
-        // If token is valid, set the decoded user object to the request for use in other routes
-        req.user = decoded;
-        next(); // proceed to the next middleware or route handler
-    });
 });
  
 const PORT =5000;
